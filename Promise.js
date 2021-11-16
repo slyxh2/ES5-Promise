e46caea8b7b4caafceb625cd3aeb06f8b92aea5b
slyxh2 <88928428+slyxh2@users.noreply.github.com>
2021-11-16 10:08:50 +0800

    then finish

--- Promise.js
+++ Promise.js
@@ -1,5 +1,8 @@
 (function () {
     function Promise(executor) {
+        if (typeof executor !== 'function') {
+            throw new TypeError('Promise resolver ' + executor + ' is not a function');
+        }
         var self = this;
         self.PromiseState = 'pending';
         self.PromiseResult = undefined;
@@ -29,24 +32,75 @@
             reject(err);
         }
     }
+    function handlePromise(x, promise, resolve, reject) {
+        if (x === promise) throw new TypeError('Chaining cycle detected for promise #<Promise>');
+        if (x !== null && typeof x === 'function' || typeof x === 'object') {
+            try {
+                var then = x.then;
+                if (typeof then === 'function') {
+                    then.call(x, function (y) {
+                        resolve(y)
+                    }, function (r) {
+                        reject(r);
+                    })
+                } else {
+                    resolve(x);
+                }
+            } catch (err) {
+                reject(err);
+            }
+        } else {
+            resolve(x);
+        }
+    }
     Promise.prototype = {
         isNative: false,
         constructor: Promise,
         then: function (onfuifilled, onrejected) {
             var self = this;
-            if (self.PromiseState === 'fulfilled') {
-                setTimeout(function () {
-                    onfuifilled(self.PromiseResult);
-                })
-            } else if (self.PromiseState === 'rejected') {
-                setTimeout(function () {
-                    onrejected(self.PromiseResult);
-                })
-            } else {
-                self.fulfilledCallbacks.push(onfuifilled);
-                self.rejectedCallbacks.push(onrejected);
-            }
-        },
+            var promise = new Promise(function (resolve, reject) {
+                if (self.PromiseState === 'fulfilled') {
+                    setTimeout(function () {
+                        var x = onfuifilled(self.PromiseResult);
+                        handlePromise(x, promise, resolve, reject);
+                    })
+                } else if (self.PromiseState === 'rejected') {
+                    setTimeout(function () {
+                        var x = onrejected(self.PromiseResult);
+                        handlePromise(x, promise, resolve, reject);
+                    })
+                } else {
+                    self.fulfilledCallbacks.push(function () {
+                        try {
+                            var x = onfuifilled(self.PromiseResult);
+                            handlePromise(x, promise, resolve, reject);
+                        } catch (err) {
+                            reject(err);
+                        }
+                    });
+                    self.rejectedCallbacks.push(function () {
+                        try {
+                            var x = onrejected(self.PromiseResult);
+                            handlePromise(x, promise, resolve, reject);
+                        } catch (err) {
+                            reject(err);
+                        }
+                    });
+                }
+            })
+            return promise;
+
+        }
+    };
+    Promise.resolve = function (value) {
+        return new Promise(function (resolve) {
+            resolve(value);
+        })
+    };
+    Promise.reject = function (reason) {
+        return new Promise(function (_, reject) {
+            reject(reason);
+        })
     }
     window.Promise = Promise;
 })()
